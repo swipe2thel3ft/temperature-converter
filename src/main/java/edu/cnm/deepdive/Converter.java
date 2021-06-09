5cc0d6acf939255f63b1a1fe523673d947b0c12d
Victor Perez <victorperez099@yahoo.com>
2021-06-09 10:24:37 -0600

    Extracted double constants and implemented  convertF2C

--- src/main/java/edu/cnm/deepdive/Converter.java
+++ src/main/java/edu/cnm/deepdive/Converter.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Converter {
 
   private static final String INPUT_PROMPT = "Please type a celsius temperature (blank to quit): ";
-  private static final String FAHERENHEIT_OUTPUT_FORMAT = "Fahrenheit temperature: %,.2f degrees%nPlease type a Celsius temperature (blank to quit):";
+  private static final String FAHRENHEIT_OUTPUT_FORMAT = "Fahrenheit temperature: %,.2f degrees%nPlease type a Celsius temperature (blank to quit):";
   private static final String PARSE_EXCEPTION_FORMAT = "Unable to parse \"%s\". Please try again: ";
+  private static final double FAHRENHEIT_PER_CELSIUS = 9.0/ 5;
+  private static final int FAHRENHEIT_FREEZING_POINT = 32;
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
@@ -15,7 +17,7 @@ public class Converter {
     while (!userInput.isEmpty()) {
       try {
         //TODO check last character of userInput; if 'f' treat as Fahrenheit; otherwise, celsius
-        System.out.printf(FAHERENHEIT_OUTPUT_FORMAT, convertC2F(Double.parseDouble(userInput)));
+        System.out.printf(FAHRENHEIT_OUTPUT_FORMAT, convertC2F(Double.parseDouble(userInput)));
       } catch (NumberFormatException e) {
         System.out.printf(PARSE_EXCEPTION_FORMAT, userInput);
       }
@@ -24,10 +26,11 @@ public class Converter {
   }
 
   public static double convertC2F(double celsius) {
-    return celsius * 9 / 5 + 32;
+    return celsius * FAHRENHEIT_PER_CELSIUS + FAHRENHEIT_FREEZING_POINT;
   }
 
   public static double convertF2C(double fahrenheit) {
-    return 0; // TODO Implement  the Fahrenheit-to-Celsius conversion formula.
+    return 5 * (fahrenheit - FAHRENHEIT_FREEZING_POINT) / FAHRENHEIT_PER_CELSIUS;
   }
+
 }
